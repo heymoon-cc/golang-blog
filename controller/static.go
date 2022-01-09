e4b51d66f167b342cae4adbea1f7e77acf193fa5
Egor <egor.bukin@iss.digital>
2022-01-09 19:40:01 +0300

    External files support

--- controller/static.go
+++ controller/static.go
@@ -1,25 +1,33 @@
 package controller
 
 import (
-  "github.com/gorilla/mux"
-  "io"
-  "mime"
-  "net/http"
-  "os"
-  "path/filepath"
+	"github.com/gorilla/mux"
+	"io"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
+func handleStatic(dir string, w http.ResponseWriter, r *http.Request) {
+	file := mux.Vars(r)["file"]
+	data, err := os.Open(dir + "/" + file)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", mime.TypeByExtension(filepath.Ext(file)))
+	_, err = io.Copy(w, data)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+}
+
 func StaticHandler(w http.ResponseWriter, r *http.Request) {
-  file := mux.Vars(r)["file"]
-  data, err := os.Open("./static/dist/" + file)
-  if err != nil {
-    http.NotFound(w, r)
-    return
-  }
-  w.Header().Set("Content-Type", mime.TypeByExtension(filepath.Ext(file)))
-  _, err = io.Copy(w, data)
-  if err != nil {
-    http.NotFound(w, r)
-    return
-  }
+	handleStatic("./static/dist", w, r)
+}
+
+func FilesHandler(w http.ResponseWriter, r *http.Request) {
+	handleStatic("./files", w, r)
 }
--- router.go
+++ router.go
@@ -11,6 +11,7 @@ func getRouter() *mux.Router {
 	r.HandleFunc("/tag/{tag}", controller.TagHandler)
 	r.HandleFunc("/article/{id}", controller.ArticleHandler).Methods("GET")
 	r.HandleFunc("/static/{file}", controller.StaticHandler).Methods("GET")
+	r.HandleFunc("/files/{file}", controller.FilesHandler).Methods("GET")
 	r.HandleFunc("/admin/create", controller.CreateArticleHandler).Methods("GET", "POST")
 	r.HandleFunc("/admin/update/{id}", controller.UpdateArticleHandler).Methods("GET", "POST")
 	return r
